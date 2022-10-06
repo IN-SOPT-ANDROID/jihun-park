5de9221fce4de9ab73f360ec73ef78cf7dd7becd
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-06 16:50:41 +0900

    feat : 로그인/회원가입 기능에 registerForActivity 적용

--- app/src/main/java/org/sopt/sample/src/login/SignInActivity.kt
+++ app/src/main/java/org/sopt/sample/src/login/SignInActivity.kt
@@ -1,43 +1,75 @@
 package org.sopt.sample.src.login
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import org.sopt.sample.R
 import org.sopt.sample.config.BaseActivity
 import org.sopt.sample.databinding.ActivitySigninBinding
 import org.sopt.sample.src.HomeActivity
 
 
 class SignInActivity : BaseActivity<ActivitySigninBinding>(ActivitySigninBinding::inflate) {
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent> //회원가입 정보를 받아오기 위한 launcher
+    private lateinit var idFromSignup: String
+    private lateinit var pwFromSignup: String
+
+    private val signupIntent = Intent(this, SignupActivity::class.java)
+    private val homeIntent = Intent(this, HomeActivity::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val signupIntent = Intent(this, SignupActivity::class.java)
-        val homeIntent = Intent(this, HomeActivity::class.java)
-
-        signIn(homeIntent)
-        signUp(signupIntent)
-
+        setResultSignUp() //회원가입 콜백
+        signIn(homeIntent) //로그인
+        signUp(signupIntent) //회원가입
 
     }
-    private fun signIn(homeIntent:Intent){
+
+    //Signup Callback
+    private fun setResultSignUp() {
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    idFromSignup = result.data?.getStringExtra("id").toString()
+                    pwFromSignup = result.data?.getStringExtra("pw").toString()
+                    CustomSnackBar(getString(R.string.signin_success_signup))
+                }
+                //로그인 실패
+                else {
+                    CustomSnackBar(getString(R.string.signin_fail_id_or_pw)).setAnchorView(binding.signInLoginBtn)
+                        .show()
+                }
+            }
+    }
+
+    private fun signIn(homeIntent: Intent) {
         //로그인 버튼 클릭
         binding.signInLoginBtn.setOnClickListener {
             //SignUp에서 받아온 id/pw와 일치하는지 체크
-            if(binding.signInIdEt.text.toString()==(intent.getStringExtra("id")) &&
-                binding.signInPwEt.text.toString()==(intent.getStringExtra("pw"))){
+            if (binding.signInIdEt.text.toString() == idFromSignup &&
+                binding.signInPwEt.text.toString() == (pwFromSignup)
+            ) {
+                homeIntent.putExtra("id", idFromSignup)
+                homeIntent.putExtra("pw", pwFromSignup)
                 startActivity(homeIntent)
             }
             //로그인 실패
-            else{
-                CustomSnackBar("ID 또는 PW를 확인해주세요!").setAnchorView(binding.signInLoginBtn).show()
+            else {
+                CustomSnackBar(getString(R.string.signin_fail_id_or_pw)).setAnchorView(binding.signInLoginBtn)
+                    .show()
             }
         }
     }
-    private fun signUp(signupIntent:Intent){
-        //회원가입 버튼 클릭
+
+    private fun signUp(signupIntent: Intent) {
+        //회원가입 버튼
         binding.signInSignUpBtn.setOnClickListener {
-            startActivity(signupIntent)
+            resultLauncher.launch(signupIntent) //데이터를 받아올 SignupActivity 실행
         }
     }
 }
--- app/src/main/java/org/sopt/sample/src/login/SignupActivity.kt
+++ app/src/main/java/org/sopt/sample/src/login/SignupActivity.kt
@@ -2,8 +2,10 @@ package org.sopt.sample.src.login
 
 import android.content.Intent
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import org.sopt.sample.R
 import org.sopt.sample.config.BaseActivity
+import org.sopt.sample.databinding.ActivityHomeBinding.inflate
 import org.sopt.sample.databinding.ActivitySignupBinding
 
 class SignupActivity : BaseActivity<ActivitySignupBinding>(ActivitySignupBinding::inflate) {
@@ -16,12 +18,12 @@ class SignupActivity : BaseActivity<ActivitySignupBinding>(ActivitySignupBinding
             if (binding.signUpIdEt.text.length in 6..10) {
                 //PW CHECK - Length Success
                 if (binding.signUpPwEt.text.length in 8..12) {
-                    showCustomToast(getString(R.string.signup_login_success))
                     //SignUp Success->HomeActivity
                     signInIntent.putExtra("id",binding.signUpIdEt.text.toString())
                     signInIntent.putExtra("pw",binding.signUpPwEt.text.toString())
                     signInIntent.putExtra("mbti",binding.signUpMbtiEt.text.toString())
-                    startActivity(signInIntent)
+                    setResult(RESULT_OK,signInIntent) //result code 및 intent 설정
+                    finish() //액티비티 종료
                 }
                 //PW CHECK - Length Fail
                 else {
