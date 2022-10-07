c60256701e5ed0bc9db0e966769dac6b6bacced9
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-08 02:34:49 +0900

    feat : 로그아웃 기능
    HomeActivity : 로그아웃 버튼 추가 및 로그아웃 기능 구현(sp clear() 및 SignInActivity start)

--- app/src/main/java/org/sopt/sample/src/HomeActivity.kt
+++ app/src/main/java/org/sopt/sample/src/HomeActivity.kt
@@ -1,31 +1,36 @@
 package org.sopt.sample.src
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.OnBackPressedCallback
 import org.sopt.sample.R
+import org.sopt.sample.config.ApplicationClass
 import org.sopt.sample.config.BaseActivity
 import org.sopt.sample.databinding.ActivityHomeBinding
+import org.sopt.sample.src.login.SignInActivity
 
 class HomeActivity : BaseActivity<ActivityHomeBinding>(ActivityHomeBinding::inflate) {
     private lateinit var mbti:String
+    private lateinit var prefs: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        prefs = ApplicationClass.sSharedPreferences
+
         mbti = intent.getStringExtra("mbti").toString()
         binding.homeNameTv.text = getString(R.string.home_user_name)
         "MBTI: $mbti".also { binding.homeMbtiTv.text = it }
-
-
-
-//        onBackPressedDispatcher.addCallback(
-//            this,
-//            object : OnBackPressedCallback(true) {
-//                override fun handleOnBackPressed() {
-//                    exitOnBackPressed()
-//                }
-//            })
+        logOut()
     }
-//    private fun exitOnBackPressed(){
-//
-//    }
 
+    /** 로그아웃 : sp를 초기화하고 SignInActivity Start */
+    private fun logOut(){
+        binding.homeLogoutBtn.setOnClickListener {
+            prefs.edit().clear().commit() //모든 저장데이터 삭제
+            showCustomToast("로그아웃 되었습니다.")
+            finish()
+            val intent = Intent(this,SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
