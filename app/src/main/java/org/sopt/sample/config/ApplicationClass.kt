6dde06df7d665a695f61c57308c6c1854eee5d67
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-09 21:47:46 +0900

    Update ApplicationClass.kt

--- app/src/main/java/org/sopt/sample/config/ApplicationClass.kt
+++ app/src/main/java/org/sopt/sample/config/ApplicationClass.kt
@@ -7,10 +7,12 @@ import org.sopt.sample.R
 //앱 최초 생성 시 sp 새로 생성
 class ApplicationClass: Application() {
     //전역변수
+
     companion object{
-        const val USER_INFO_ID = "id" //상수화 -> 가독성 및 유지보수
-        const val USER_INFO_PW = "pw" //상수화 -> 가독성 및 유지보수
-        const val USER_INFO_MBTI = "mbti" //상수화 -> 가독성 및 유지보수
+        //상수화 -> 가독성 및 유지보수
+        const val USER_INFO_ID = "id"
+        const val USER_INFO_PW = "pw"
+        const val USER_INFO_MBTI = "mbti"
         lateinit var sSharedPreferences: SharedPreferences
     }
     override fun onCreate() {
