1dda2e4e7c3394b290e498dda532b1774431b871
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-12 02:41:46 +0900

    chore : 기존 기능 삭제(초기 tv,iv init 및 로그아웃)

--- app/src/main/java/org/sopt/sample/presentation/MainActivity.kt
+++ app/src/main/java/org/sopt/sample/presentation/MainActivity.kt
@@ -11,27 +11,23 @@ import org.sopt.sample.util.const.USER_INFO_MBTI
 import org.sopt.sample.util.extensions.showToast
 
 class MainActivity : BindingActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
-    private lateinit var mbti:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        init()
-        logOutBtnListener()
-    }
-    private fun init(){
-        mbti = intent.getStringExtra(USER_INFO_MBTI).toString()
-        binding.homeNameTv.text = getString(R.string.home_user_name)
-        binding.homeMbtiTv.text = "${getString(R.string.home_mbti_tv)} $mbti"
+//        logOutBtnListener()
+        val currentFragment = supportFragmentManager.findFragmentById()
+
 
     }
 
-    /** 로그아웃 : sp를 초기화하고 SignInActivity Start */
-    private fun logOutBtnListener(){
-        binding.homeLogoutBtn.setOnClickListener {
-            ApplicationClass.sSharedPreferences.edit().clear().commit() //모든 저장데이터 삭제
-            this.showToast("로그아웃 되었습니다.")
-            finish()
-            val intent = Intent(this,SignInActivity::class.java)
-            startActivity(intent)
-        }
-    }
+
+//    /** 로그아웃 : sp를 초기화하고 SignInActivity Start */
+//    private fun logOutBtnListener(){
+//        binding.homeLogoutBtn.setOnClickListener {
+//            ApplicationClass.sSharedPreferences.edit().clear().commit() //모든 저장데이터 삭제
+//            this.showToast("로그아웃 되었습니다.")
+//            finish()
+//            val intent = Intent(this,SignInActivity::class.java)
+//            startActivity(intent)
+//        }
+//    }
 }
