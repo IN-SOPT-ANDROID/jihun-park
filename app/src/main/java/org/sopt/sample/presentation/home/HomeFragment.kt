dac2413125fe456a00db7e753fe61737cf6fef7b
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-26 21:57:08 +0900

    feat : Splash 화면 추가 및 애니메이션  적용

--- app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
@@ -1,85 +1,96 @@
 package org.sopt.sample.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import org.sopt.sample.R
+import org.sopt.sample.application.ApplicationClass
 import org.sopt.sample.base.BindingFragment
 import org.sopt.sample.databinding.FragmentHomeBinding
-import org.sopt.sample.presentation.home.data.HomeRecycleData
 import org.sopt.sample.presentation.home.data.HomeRepoContentData
 import org.sopt.sample.presentation.home.data.HomeRepoTitleData
 import org.sopt.sample.presentation.home.diffUtilAdapter.HomeRepoListAdapter
+import org.sopt.sample.presentation.login.SignInActivity
 import org.sopt.sample.util.RecyclerDecorationHeight
+import org.sopt.sample.util.extensions.showToast
 
 class HomeFragment :
     BindingFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home) {
     private val homeRecycleList by lazy {
         listOf(
-        HomeRepoTitleData(
-            titleName = "지훈이의 레포지터리"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Park"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Lee"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Han"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Seok"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Kim"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Woo"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Hwang"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Jegal"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Sunwoo"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Kang"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Choi"
-        ),
-        HomeRepoContentData(
-            profileImg = R.drawable.home_profile_img_bino,
-            repoName = "IN_SOPT",
-            authorName = "Jihun Son"
+            HomeRepoTitleData(
+                titleName = "지훈이의 레포지터리"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Park"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Lee"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Han"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Seok"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Kim"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Woo"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Hwang"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Jegal"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Sunwoo"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Kang"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Choi"
+            ),
+            HomeRepoContentData(
+                profileImg = R.drawable.home_profile_img_bino,
+                repoName = "IN_SOPT",
+                authorName = "Jihun Son"
+            )
         )
-    )}
-    private val adapter by lazy { HomeRepoListAdapter(requireContext()).apply { submitList(homeRecycleList) } }
+    }
+    private val adapter by lazy {
+        HomeRepoListAdapter(requireContext()).apply {
+            submitList(
+                homeRecycleList
+            )
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -88,8 +99,20 @@ class HomeFragment :
         super.onViewCreated(view, savedInstanceState)
         initAdapter()
         heightGapRv()
-
+        logOutBtnListener()
     }
+
+    /** 로그아웃 : sp를 초기화하고 SignInActivity Start */
+    private fun logOutBtnListener() {
+        binding.homeLogout.setOnClickListener {
+            ApplicationClass.sSharedPreferences.edit().clear().commit() //모든 저장데이터 삭제
+            this.showToast("로그아웃 되었습니다.")
+            val intent = Intent(activity, SignInActivity::class.java)
+            startActivity(intent)
+            activity?.finish()
+        }
+    }
+
 
     private fun initAdapter() {
         binding.homeRecyclerRepo.adapter = adapter
@@ -103,8 +126,9 @@ class HomeFragment :
         }
 
     }
+
     //리사이클러 뷰 간격(높이)
-    private fun heightGapRv(){
+    private fun heightGapRv() {
         binding.homeRecyclerRepo.addItemDecoration(RecyclerDecorationHeight(80))
     }
 
--- app/src/main/java/org/sopt/sample/splash/SplashActivity.kt
+++ app/src/main/java/org/sopt/sample/splash/SplashActivity.kt
@@ -1,13 +1,70 @@
 package org.sopt.sample.splash
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.animation.AlphaAnimation
+import org.sopt.sample.R
+import org.sopt.sample.application.ApplicationClass
 import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivitySplashBinding
+import org.sopt.sample.presentation.MainActivity
+import org.sopt.sample.presentation.login.SignInActivity
+import org.sopt.sample.presentation.search.SearchFragment
+import org.sopt.sample.util.const.USER_INFO_ID
+import org.sopt.sample.util.const.USER_INFO_MBTI
+import org.sopt.sample.util.const.USER_INFO_PW
+import org.sopt.sample.util.extensions.showToast
 
 class SplashActivity : BindingActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        //sp에 로그인 정보가 있으면, MainActivity로
+        //없으면 SignInActivity로
+        Thread {
+            handler.postDelayed({
+                autoLogin()
+            },3000)
+        }.start()
+        overridePendingTransition(R.anim.fadein, R.anim.fadeout)
     }
 
+    /** 자동 로그인 여부 */
+    private fun isAutoLogin(): Boolean {
+        //로그인 성공 했을 때 저장해놓은 정보를 HomeActivity에 보내면서 자동 로그인
+        val id = ApplicationClass.sSharedPreferences.getString(USER_INFO_ID, null)
+        val pw = ApplicationClass.sSharedPreferences.getString(USER_INFO_PW, null)
+        return (id != null && pw != null)
+    }
+
+    //3초 로딩 후 SigninActivity
+    private fun autoLogin() {
+        if (isAutoLogin()) {
+            Log.d(SearchFragment::class.java.simpleName,"자동 로그인 중")
+            val intent = Intent(this, MainActivity::class.java)
+            intent.apply {
+                putExtra(
+                    USER_INFO_ID,
+                    ApplicationClass.sSharedPreferences.getString(USER_INFO_ID, null)
+                )
+                putExtra(
+                    USER_INFO_PW,
+                    ApplicationClass.sSharedPreferences.getString(USER_INFO_PW, null)
+                )
+                putExtra(
+                    USER_INFO_MBTI,
+                    ApplicationClass.sSharedPreferences.getString(USER_INFO_MBTI, null)
+                )
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                this@SplashActivity.showToast(getString(R.string.signin_auto_login_complete))
+                startActivity(this)
+            }
+        } else {
+            Log.d(SearchFragment::class.java.simpleName,"로그인 화면 이동중")
+            startActivity(Intent(this, SignInActivity::class.java))
+        }
+    }
 }
