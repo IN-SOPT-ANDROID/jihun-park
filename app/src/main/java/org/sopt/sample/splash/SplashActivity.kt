d2e094c2db6ebfd153b53828be714188591f4f59
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-10-26 15:11:55 +0900

    add :SplashActivity 추가

---
+++ app/src/main/java/org/sopt/sample/splash/SplashActivity.kt
@@ -0,0 +1,13 @@
+package org.sopt.sample.splash
+
+import android.os.Bundle
+import org.sopt.sample.base.BindingActivity
+import org.sopt.sample.databinding.ActivitySplashBinding
+
+class SplashActivity : BindingActivity<ActivitySplashBinding>(ActivitySplashBinding::inflate) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+}
