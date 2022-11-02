53ed6ace0274782d8258cec3991b34f8229bb412
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-02 23:48:57 +0900

    chore : SplashViewModel 추가 및 뷰와 로직의 분리
    자동 로그인 관련 로직을 ViewModel로 분리

--- app/src/main/java/org/sopt/sample/splash/SplashActivity.kt
+++ app/src/main/java/org/sopt/sample/splash/SplashActivity.kt
@@ -2,75 +2,58 @@ package org.sopt.sample.splash
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
 import androidx.activity.viewModels
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.sopt.sample.R
-import org.sopt.sample.application.ApplicationClass
 import org.sopt.sample.base.BindingActivity
 import org.sopt.sample.databinding.ActivitySplashBinding
 import org.sopt.sample.presentation.MainActivity
 import org.sopt.sample.presentation.login.SignInActivity
-import org.sopt.sample.presentation.search.SearchFragment
-import org.sopt.sample.util.const.*
+import org.sopt.sample.util.EventObserver
 import org.sopt.sample.util.extensions.showToast
 
 class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
     private val viewModel: SplashViewModel by viewModels()
-    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //sp에 로그인 정보가 있으면, MainActivity로
-        //없으면 3초 로딩 후 SigninActivity
-        Thread {
-            handler.postDelayed({
-                autoLogin()
-            }, 3000)
-        }.start()
-
+        setAnimation() //화면 전환 애니메이션
         //뷰모델 초기화
         binding.viewModel = viewModel
         //Splash Activity의 별도 코드실행 없이 생명주기를 다룰 수 있도록 함.
         binding.lifecycleOwner = this
-        setAnimation()
+
+        addObserver()
     }
-    private fun setAnimation(){
+
+    private fun setAnimation() {
         overridePendingTransition(R.anim.fadein, R.anim.fadeout)
     }
 
-    /** 자동 로그인 여부 */
-    private fun isAutoLogin(): Boolean {
-        //로그인 성공 했을 때 저장해놓은 정보를 HomeActivity에 보내면서 자동 로그인
-        val id = ApplicationClass.sSharedPreferences.getString(AUTO_LOGIN_ID, null)
-        val pw = ApplicationClass.sSharedPreferences.getString(AUTO_LOGIN_PW, null)
-        return (id != null && pw != null)
-    }
-
-    private fun autoLogin() {
-        if (isAutoLogin()) {
-            Log.d(SearchFragment::class.java.simpleName, "자동 로그인 중")
-            val intent = Intent(this, MainActivity::class.java)
-            intent.apply {
-                putExtra(
-                    USER_INFO_ID,
-                    ApplicationClass.sSharedPreferences.getString(USER_INFO_ID, null)
-                )
-                putExtra(
-                    USER_INFO_PW,
-                    ApplicationClass.sSharedPreferences.getString(USER_INFO_PW, null)
-                )
-                putExtra(
-                    USER_INFO_MBTI,
-                    ApplicationClass.sSharedPreferences.getString(USER_INFO_MBTI, null)
-                )
-                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                this@SplashActivity.showToast(getString(R.string.signin_auto_login_complete))
-                startActivity(this)
+    //뷰모델의 StateFlow isSignedUser 관찰
+    //뷰모델에서 checkSignedUser함수의 호출에 따라 isSignedUser의 값이 변경되면, 이후 코드 실행
+    private fun addObserver() {
+        //LiveData.observe - 뷰가 STOPPED 상태가 되면소비자를 자동으로 등록 취소한다.
+        //StateFlow - 자동으로 중지하지 않기 때문에, lifecycleScope로 생명주기를 인지시켜주거나, lifecycle-livedata-ktx의 확장함수 asLiveData로 해결 가능
+        viewModel.isSignedUser.asLiveData().observe(this, EventObserver { isSigned ->
+            lifecycleScope.launch {
+                delay(2000)
+                moveToNext(isSigned)
+                finish()
             }
-        } else {
-            Log.d(SearchFragment::class.java.simpleName, "로그인 화면 이동중")
-            startActivity(Intent(this, SignInActivity::class.java))
+        })
+    }
+    private fun moveToNext(isSigned: Boolean) {
+        //isSigned(자동 로그인 여부)에 따라 MainActivity 또는 SignInActivity로 StartActivity
+        when (isSigned) {
+            true -> {
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                this@SplashActivity.showToast(getString(R.string.signin_auto_login_complete))
+            }
+            false -> startActivity(Intent(this@SplashActivity, SignInActivity::class.java))
         }
     }
 }
+
--- app/src/main/java/org/sopt/sample/splash/SplashViewModel.kt
+++ app/src/main/java/org/sopt/sample/splash/SplashViewModel.kt
@@ -1,6 +1,29 @@
 package org.sopt.sample.splash
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import org.sopt.sample.application.ApplicationClass
+import org.sopt.sample.util.Event
 
-class SplashViewModel:ViewModel() {
+class SplashViewModel : ViewModel() {
+    //backing property를 이용하여 내부에서는 가변적이지만, 외부로 넘겨줄 때에는 불변 StateFlow로 변경하여 반환한다.
+    //LiveData와는 달리 StateFlow는 초기값을 필요로 한다.
+    private var _isSignedUser = MutableStateFlow<Event<Boolean>>(Event(false))
+    val isSignedUser: StateFlow<Event<Boolean>> get() = _isSignedUser
+
+    init {
+        viewModelScope.launch {
+            checkSignedUser()
+        }
+    }
+
+    /** 자동 로그인 여부 체크*/
+    private suspend fun checkSignedUser() {
+        _isSignedUser.value = Event(ApplicationClass.getInstance().getUserManager().getUserInfo() != null)
+    }
 }
