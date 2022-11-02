a7adf2ac1ad97ab42442156909e059d4939fc57b
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-02 23:42:59 +0900

    chore : 단순 refactoring

--- app/src/main/java/org/sopt/sample/util/extensions/ViewExt.kt
+++ app/src/main/java/org/sopt/sample/util/extensions/ViewExt.kt
@@ -6,8 +6,7 @@ import com.google.android.material.snackbar.Snackbar
 //확장할 클래스 - View 클래스
 //View 클래스에 속한 인스턴스 객체 - this
 //함수 내부에서는 binding.root 키워드로 수신 객체 멤버 사용
-
-fun View.makeSnackBar(message:String, isShort: Boolean=true): Snackbar {
+fun View.makeSnackBar(message: String, isShort: Boolean = true): Snackbar {
     val duration = if (isShort) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_LONG
-    return Snackbar.make(this,message,duration)
+    return Snackbar.make(this, message, duration)
 }
