acd92d1cb9deb084497dae75ebfc0ed2cfe6b965
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-03 11:22:22 +0900

    Update SplashViewModel.kt

--- app/src/main/java/org/sopt/sample/splash/SplashViewModel.kt
+++ app/src/main/java/org/sopt/sample/splash/SplashViewModel.kt
@@ -1,7 +1,5 @@
 package org.sopt.sample.splash
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
