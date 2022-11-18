f2a1a06ca8e15655fc60c8accf37b11d093e334d
Larry7939 <70442964+Larry7939@users.noreply.github.com>
2022-11-18 23:13:01 +0900

    [feat] 리사이클러뷰 아이템 간 간격 조절

--- app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/sample/presentation/home/HomeFragment.kt
@@ -8,6 +8,7 @@ import org.sopt.sample.base.BindingFragment
 import org.sopt.sample.databinding.FragmentHomeBinding
 import org.sopt.sample.presentation.common.ViewModelFactory
 import org.sopt.sample.presentation.home.adapter.HomeUserListAdapter
+import org.sopt.sample.util.RecyclerDecorationHeight
 
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
@@ -21,6 +22,7 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
         viewModel.loadUserList(2) //page 수 Query
 
         addObserver(adapter)
+        heightGapRv()
     }
     //load 성공 여부 observe 후 adapter로 userList 전달
     private fun addObserver(adapter:HomeUserListAdapter){
@@ -29,6 +31,10 @@ class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home
                 viewModel.userList.value?.let { it1 -> adapter.submitUserList(it1) }
             }
         }
+    }
+    //리사이클러 뷰 간격(높이)
+    private fun heightGapRv() {
+        binding.homeRv.addItemDecoration(RecyclerDecorationHeight(80))
     }
 
 //    /** 로그아웃 : sp를 초기화하고 SignInActivity Start */
